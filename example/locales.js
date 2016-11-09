018e6f68401ee2efc6ac4afd3a2cda8fad3b7cca
EGOIST <0x142857@gmail.com>
2016-11-09 20:23:32 +0800

    use require.context to load all locales

--- example/locales.js
+++ example/locales.js
@@ -1,17 +1,12 @@
-export default {
-  'en-US': require('../locales/en-US.json'),
-  'zh-CN': require('../locales/zh-CN.json'),
-  'zh-TW': require('../locales/zh-TW.json'),
-  'hu-HU': require('../locales/hu-HU.json'),
-  'bg-BG': require('../locales/bg-BG.json'),
-  'de-DE': require('../locales/de-DE.json'),
-  'es-ES': require('../locales/es-ES.json'),
-  'pt-BR': require('../locales/pt-BR.json'),
-  'fr-FR': require('../locales/fr-FR.json'),
-  'it-IT': require('../locales/it-IT.json'),
-  'ru-RU': require('../locales/ru-RU.json'),
-  'fa-IR': require('../locales/fa-IR.json'),
-  'ja-JP': require('../locales/ja-JP.json'),
-  'tr-TR': require('../locales/tr-TR.json'),
-  'da-DK': require('../locales/da-DK.json')
+function requireAll(requireContext) {
+  const keys = requireContext.keys()
+  return keys
+    .reduce((current, next) => {
+      current[next.substr(2, 5)] = requireContext(next)
+      return current
+    }, {})
 }
+
+const locales = requireAll(require.context("../locales", true, /\.json$/));
+
+export default locales
