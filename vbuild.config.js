0ed5c117390cd20466d26b6444f8ed3148e499f5
EGOIST <0x142857@gmail.com>
2017-02-17 23:19:26 +0800

    tweak vbuild config

--- vbuild.config.js
+++ vbuild.config.js
@@ -1,15 +1,11 @@
 module.exports = (options, req) => ({
   entry: 'example',
   dist: 'dist-demo',
+  homepage: '/vue-timeago/',
   html: {
     title: 'Vue Timeago'
   },
   postcss: [
     require('postcss-nested')
-  ],
-  webpack: {
-    output: {
-      publicPath: '/vue-timeago/'
-    }
-  }
+  ]
 })
