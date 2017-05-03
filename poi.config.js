fc7195ed2ac20240a5cb1031fe7f31bed5b45688
EGOIST <0x142857@gmail.com>
2017-05-03 22:46:12 +0800

    use poi

--- poi.config.js
+++ poi.config.js
@@ -2,9 +2,6 @@ module.exports = (options, req) => ({
   entry: 'example',
   dist: 'dist-demo',
   homepage: '/vue-timeago/',
-  html: {
-    title: 'Vue Timeago'
-  },
   postcss: [
     require('postcss-nested')
   ]
